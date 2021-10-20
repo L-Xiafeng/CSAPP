ab055c26db996c92e5dcbf0ca58e102428dfa15e
root <L-Xiafeng>
2021-10-20 13:36:57 +0800

    显式链表，初次匹配

--- mm.c
+++ mm.c
@@ -48,7 +48,7 @@ team_t team = {
 #define WSIZE 4             /* Word and header/footer size (bytes) */
 #define DSIZE 8             /* Double word size (bytes) */
 #define CHUNKSIZE (1<<12)   /* Extend heap by this amount (bytes) */
-
+#define MINBLOCKSIZE 16
 #define MAX(x, y) ((x) > (y)? (x) : (y))
 
 /* Pack a size and allocated bit into a word */
@@ -69,27 +69,92 @@ team_t team = {
 /* Given block ptr bp, compute address of next and previous blocks */
 #define NEXT_BLKP(bp)  ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
 #define PREV_BLKP(bp)  ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
+
+/* SET/GET PREV/SUCC */
+#define SET_PREV(bp,val)    (*(unsigned int *)(bp) =(val))
+#define SET_SUCC(bp,val)    (*((unsigned int *)(bp)+1) =(val))
+#define GET_PREV(bp)        (*(unsigned int *)(bp))
+#define GET_SUCC(bp)    (*((unsigned int *)(bp)+1))
+
 static void* extend_heap(size_t words);
 static void* coalesce(void *bp);
 static void* find_fit(size_t asize);
+static void *first_fit(size_t asize);
 static void place(void* bp, size_t asize);
+static void remove_from_free_list(void* bp);
+static void insert_to_free_list(void* bp);
+static void* init_free_node(void* bp);
 static void* heap_listp;
+static void* free_list_head;
 
+
+/*
+ * remove_from_free_list - remove the block from free list
+ * if there is only one block, set free_list_head = NULL
+ */ 
+static void remove_from_free_list(void *bp)
+{
+    void* pre = GET_PREV(bp);
+    void* succ = GET_SUCC(bp);
+    SET_PREV(bp,0);
+    SET_SUCC(bp,0);
+    //是空闲链表中唯一一个
+    if (pre==NULL&&succ==NULL){
+        free_list_head=NULL;
+    }
+    //是最后一个
+    else if (succ==NULL){
+        SET_SUCC(pre,0);
+    }
+    //是第一个
+    else if (pre==NULL){
+        free_list_head=succ;
+        SET_PREV(succ,0);
+    }
+    //在中间
+    else {
+        SET_SUCC(pre,succ);
+        SET_PREV(succ,pre);
+    }
+}
+/* 
+ * insert_to_free_list - insert block bp at the head of the free list
+ */
+static void insert_to_free_list(void* bp){
+    if(bp==NULL) return;
+    if(free_list_head==NULL){
+        free_list_head = bp;
+        return;
+    }
+    SET_PREV(free_list_head,bp);
+    SET_SUCC(bp,free_list_head);
+    free_list_head = bp;
+}
+static void* init_free_node(void* bp){
+    SET_PREV(bp,0);
+    SET_SUCC(bp,0);
+    return bp;
+}
+//合并物理位置相邻空闲,传入的bp来自free或者extend_heep后得到的未加入空闲链表的块
 static void* coalesce(void* bp){
     size_t pre_alloced = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    void* pre = PREV_BLKP(bp);
+    void* succ = NEXT_BLKP(bp);
     size_t next_alloced = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
     size_t size = GET_SIZE(HDRP(bp));
     if(pre_alloced&&next_alloced){
-        return bp;
+        
     }
     //后一个块空闲
     else if(pre_alloced && !next_alloced){
+        remove_from_free_list(succ);
         size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
         PUT(HDRP(bp),PACK(size,0));//HEAD设置空闲块大小和是否使用
         PUT(FTRP(bp),PACK(size,0));//FOOT设置空闲块大小和是否使用
     }
     //前一个块空闲
     else if(!pre_alloced && next_alloced){
+                remove_from_free_list(pre);
         size += GET_SIZE(HDRP(PREV_BLKP(bp)));
         // PUT(HDRP(PREV_BLKP(bp)),PACK(size,0));//HEAD设置空闲块大小和是否使用
         // PUT(FTRP(bp),PACK(cru_size,0));//FOOT设置空闲块大小和是否使用
@@ -99,11 +164,14 @@ static void* coalesce(void* bp){
     }
     //前后块空闲
     else if(!pre_alloced && !next_alloced){
+        remove_from_free_list(pre);
+        remove_from_free_list(succ);
         size += GET_SIZE(HDRP(NEXT_BLKP(bp)))+GET_SIZE(FTRP(PREV_BLKP(bp)));
         PUT(HDRP(PREV_BLKP(bp)),PACK(size,0));//HEAD设置空闲块大小和是否使用
         PUT(FTRP(NEXT_BLKP(bp)),PACK(size,0));//FOOT设置空闲块大小和是否使用
         bp=PREV_BLKP(bp);
     }
+    insert_to_free_list(bp);
     return bp;
 }
 
@@ -120,36 +188,46 @@ static void* extend_heap(size_t words){
     /* Initialize free block header/footer and the epilogue header */
     PUT(HDRP(bp),PACK(size,0));//head设置空闲块大小和是否使用
     PUT(FTRP(bp),PACK(size,0));//foot设置空闲块大小和是否使用
+    bp = init_free_node(bp);
     PUT(HDRP(NEXT_BLKP(bp)),PACK(0,1));//设置结尾块
-    
     return coalesce(bp);
 }
 /*
  * find_fit - use first fit strategy to find an empty block.
  */
 static void *find_fit(size_t asize){
-    for(char * ptr = heap_listp ; GET_SIZE(HDRP(ptr)) > 0 ; ptr = NEXT_BLKP(ptr) ){
-        if( !GET_ALLOC(HDRP(ptr)) && (GET_SIZE(HDRP(ptr)) >= asize) )
+    return first_fit(asize);
+}
+
+static void *first_fit(size_t asize){
+    for(void * ptr = free_list_head ; ptr!=NULL  ; ptr = GET_SUCC(ptr) ){
+        if(GET_SIZE(HDRP(ptr)) >= asize){
             return ptr;
+        }
     }
     return NULL;
 }
 
+//set the block to be alloced and if necessary split the block
 static void  place(void *bp,size_t asize){
     size_t csize = GET_SIZE(HDRP(bp));
-    if( (csize-asize) >=2*ALIGNMENT){
+    remove_from_free_list(bp);
+    if( (csize-asize) >=MINBLOCKSIZE){
         size_t newsize =csize-asize;
         PUT(HDRP(bp),PACK(asize,1));
         PUT(FTRP(bp),PACK(asize,1));
         bp = NEXT_BLKP(bp);
         PUT(HDRP(bp),PACK(newsize,0));
         PUT(FTRP(bp),PACK(newsize,0));
+        bp = init_free_node(bp);
+        coalesce(bp);
     }
     else{
         PUT(HDRP(bp),PACK(csize,1));
         PUT(FTRP(bp),PACK(csize,1));
     }
 }
+
 /* 
  * mm_init - initialize the malloc package.
  */
@@ -163,26 +241,11 @@ int mm_init(void)
     PUT(heap_listp + (2*WSIZE),PACK(DSIZE,1));//序言块脚部设置块大小为2*WSIZE 且已经被分配
     PUT(heap_listp + (3*WSIZE),PACK(0,1));//结尾块
     heap_listp+= (2*WSIZE);
+    free_list_head=NULL;
     if( extend_heap(CHUNKSIZE/WSIZE) == NULL)
         return -1;
     return 0;
 }
-
-/* 
- * mm_malloc - Allocate a block by incrementing the brk pointer.
- *     Always allocate a block whose size is a multiple of the alignment.
- */
-// void *mm_malloc(size_t size)
-// {
-//     int newsize = ALIGN(size + SIZE_T_SIZE);
-//     void *p = mem_sbrk(newsize);
-//     if (p == (void *)-1)
-// 	    return NULL;
-//     else {
-//         *(size_t *)p = size;
-//         return (void *)((char *)p + SIZE_T_SIZE);
-//     }
-// }
 
 void *mm_malloc(size_t size)
 {
@@ -213,6 +276,7 @@ void *mm_malloc(size_t size)
     place(bp, asize);
     return bp;
 }
+
 /*
  * mm_free - Freeing a block does nothing.
  */
@@ -221,6 +285,7 @@ void mm_free(void *bp)
     size_t size = GET_SIZE(HDRP(bp));
     PUT(HDRP(bp),PACK(size,0));
     PUT(FTRP(bp),PACK(size,0));
+    bp = init_free_node(bp);
     coalesce(bp);
 }
 
@@ -255,7 +320,6 @@ void *mm_realloc(void *ptr, size_t size)
     // mm_free(oldptr);
     return newptr;
 }
-
 
 
 
